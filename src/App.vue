94d410292e8148fc1948ababe09ac7c01984ed92
Sadeq <abdi.71sadeq@gmail.com>
2025-06-27 15:25:46 +0330

    change App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,51 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div id="app">
+    <h1>My Todo App</h1>
+   
+  </div>
+  <ul>
+  <li v-for="task in tasks" :key="task.id">
+    {{ task.text }} - {{ task.completed ? '✅' : '❌' }}
+  </li>
+  <ul>
+  <li v-for="task in tasks" :key="task.id" @click="task.completed = !task.completed">
+    {{ task.text }} - {{ task.completed ? '✅' : '❌' }}
+  </li>
+</ul>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+</ul>
+<input v-model="newTaskText" placeholder="تسک جدید را بنویس" />
+<button @click="addTask">افزودن</button>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
 
-  <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<script setup>
+
+function addTask () {
+  if (!newTaskText.value.trim()) return
+
+  tasks.value.push({
+    id: Date.now(),
+    text: newTaskText.value,
+    completed:false
+  })
+  newTaskText.value = ''
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+const newTaskText = ref('')
+import { ref } from 'vue'
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+const tasks = ref([
+  { id: 1, text: 'اولین تسک', completed: false }
+])
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
+</script>
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+<style>
+#app {
+  max-width: 500px;
+  margin: auto;
+  padding: 20px;
 }
 </style>
